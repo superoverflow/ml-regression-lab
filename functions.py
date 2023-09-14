e33ca0c0a66499aed75f8bfbd27c0632f654b8f1
Cyrus.Tang <cyrus.tang@superoverflow.com>
2023-09-14 21:46:38 +0100

    adjust data set

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.typing import ArrayLike
 from functools import reduce
 
 
@@ -6,7 +7,7 @@ def generate_data(size: int):
     pass
 
 
-def generate_net_zero_series(size: int):
+def generate_net_zero_series(size: int) -> ArrayLike:
     pos_series = abs(np.random.randn(size // 2))
     neg_series = -1 * pos_series
 
@@ -17,10 +18,44 @@ def generate_net_zero_series(size: int):
     np.random.shuffle(series)
     return series
 
-def accumulate_series(series):
+def generate_data_serieses(size: int) -> tuple["np.ndarray", "np.ndarray"]:
+    pos_series = abs(np.random.randn(size // 2))
+    neg_series = -1 * pos_series
+
+    if size % 2 == 1:
+        neg_series = np.concatenate(neg_series[:-1], neg_series[-1] / 2)
+    
+    return (pos_series, neg_series)
+
+def accumulate_series(series: "np.ndarray") -> "np.ndarray":
     new_series = reduce(lambda a, s: [*a, a[-1] + s], series, [0])
     new_series.remove(0)
     return new_series
+
+def arrange_series(
+        pos_series: "np.ndarray", 
+        neg_series: "np.ndarray"
+) -> "np.ndarray":
+    pos_items = pos_series.tolist()
+    neg_items = neg_series.tolist()
+    result = []
+    while pos_items:
+        if np.random.randn() < 0.002:
+            number = pos_items.pop()
+            result.append(number)
+        else:
+            if sum(result) <= 0:
+                continue
+            else:
+                number = neg_items.pop()
+                result.append(number)
+            
+    result.extend(neg_items)
+    return np.array(result)
+
+    
+
+
 
 
 def show_data():
--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,5 @@
-from functions import generate_net_zero_series
-from math import isclose
+from functions import generate_net_zero_series, arrange_series, accumulate_series
+import numpy as np
 
 def test_generate_net_zero_series_should_net_zero():
     series = generate_net_zero_series(10)
@@ -9,3 +9,17 @@ def test_generate_net_zero_series_should_net_zero():
 def test_generate_net_zero_series_should_have_length_size():
     series = generate_net_zero_series(20)
     assert len(series) == 20
+
+
+def test_arrange_series_should_sum_to_zero():
+    pos_series = np.array([0.1,  0.2, 0.3])
+    neg_series = np.array([-0.3, -0.1, -0.2])
+    result = arrange_series(pos_series, neg_series)
+    assert abs(sum(result)) < 1e-15
+
+def test_arrange_series_should_always_greater_than_zero():
+    pos_series = np.array([0.1,  0.2, 0.3])
+    neg_series = np.array([-0.3, -0.1, -0.2])
+    arranged_series = arrange_series(pos_series, neg_series)
+    result = accumulate_series(arranged_series)
+    assert any(item > 0 for item in result)
